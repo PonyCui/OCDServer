81f225cdd1800f75eccc9fe387425a47d71e4894
ponycui <PonyCui_Home@saitekiiMac.local>
2015-06-12 23:42:48 +0800

    add vi

--- dashboard/modules/device/finder.js
+++ dashboard/modules/device/finder.js
@@ -1,4 +1,6 @@
 var currnet_finder_device_identifier = '';
+var current_finder_under_viMode = false;
+var current_finder_viMode_filePath = '';
 
 var FinderService = function() {
 
@@ -15,6 +17,19 @@ var FinderService = function() {
     }
 
     this.sendShell = function(argShell) {
+        if (current_finder_under_viMode) {
+            if (argShell == ':w') {
+                //save
+                service.pub.pubMessage('finder', 'saveViContent', {deviceIdentifier: currnet_finder_device_identifier, filePath:current_finder_viMode_filePath, fileContent:$('#finder_vi_screen').val()});
+            }
+            if (argShell == ':q') {
+                //exit
+                current_finder_under_viMode = false;
+                $('#finder_cmd_screen').show();
+                $('#finder_vi_screen').hide();
+            }
+            return;
+        }
         if (argShell == 'clear') {
             var text = $('#finder_cmd_screen').val('');
             return;
@@ -29,6 +44,16 @@ var FinderService = function() {
             var textarea = document.getElementById('finder_cmd_screen');
             textarea.scrollTop = textarea.scrollHeight;
             this.sendRequireSubPaths();
+        }
+    }
+
+    this.viMode = function(params) {
+        if (params.deviceIdentifier == currnet_finder_device_identifier) {
+            current_finder_under_viMode = true;
+            current_finder_viMode_filePath = params.filePath;
+            $('#finder_vi_screen').val(params.fileContent);
+            $('#finder_cmd_screen').hide();
+            $('#finder_vi_screen').show();
         }
     }
 }
