58191fd1e0fdedb2b5ddad28521a1391be60605e
ponycui <PonyCui_Home@saitekiiMac.local>
2015-06-13 13:20:43 +0800

    remove debug

--- application/controllers/pubsub.php
+++ application/controllers/pubsub.php
@@ -14,7 +14,7 @@ class PubSub extends CI_Controller
     }
 
     public function index()
-    {die('1');
+    {
         if (defined("SAE_TMP_PATH")) {
             //Channel
             $client_id = md5(uniqid(mt_rand(0, 9999999999)));
